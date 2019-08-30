3b1ad88e211344cb5bf6dac6eea524fa97bee958
José Molina Colmenero <jmolinacolmenero@protonmail.com>
2019-08-30 18:59:29 +0200

    Add DirectoryEntry

--- src/models/directoryentry.rs
+++ src/models/directoryentry.rs
@@ -1,8 +1,14 @@
+use crate::types::ipfs::IpfsHash;
+
 #[derive(Debug)]
 pub struct DirectoryEntry {
-
-}
-
-impl DirectoryEntry {
-
+    pub parent: IpfsHash,
+    pub path: IpfsHash,
+    pub content: IpfsHash,
+    pub flags: i32,
+    pub size: i32,
+    pub mode: i32,
+    pub mtime: i32,
+    pub name: String,
+    pub symlink: String,
 }
--- src/models/mod.rs
+++ src/models/mod.rs
@@ -2,3 +2,4 @@ pub mod manifest;
 pub mod repository;
 pub mod catalog;
 pub mod revision;
+pub mod directoryentry;
