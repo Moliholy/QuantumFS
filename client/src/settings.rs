76dd52aa2bbc6faba747b6d80bc4697bd09f44bf
José Molina Colmenero <jmolinacolmenero@protonmail.com>
2019-09-02 23:53:52 +0200

    Do no load config in ~/.qfs/settings if config file is provided

--- client/src/settings.rs
+++ client/src/settings.rs
@@ -13,17 +13,19 @@ fn init() -> Config {
         .set("web3", "http://127.0.0.1:8545").unwrap()
         .set("ipfs_gateway_server", "127.0.0.1").unwrap()
         .set("ipfs_gateway_port", 5001).unwrap();
-    // load settings in ~/.qfs/settings
-    config.merge(config::File::with_name("~/.qfs/settings")).unwrap();
-
-    // Add settings from the environment (with a prefix of QFS)
-    config.merge(config::Environment::with_prefix("QFS_")).unwrap();
 
     // Add the custom configuration file, if present
     if let Some(config_file) = ARGS.value_of("config") {
         config.merge(config::File::with_name(config_file))
             .expect(format!("Error loading the configuration from {}", config_file).as_str());
+    } else {
+        // load settings in ~/.qfs/settings, if present
+        config.merge(config::File::with_name("~/.qfs/settings"));
     }
+
+    // Add settings from the environment (with a prefix of QFS)
+    config.merge(config::Environment::with_prefix("QFS_")).unwrap();
+
     // Add the client's ethereum address if passed as a parameter
     if let Some(address) = ARGS.value_of("address") {
         config.set("address", address).unwrap();
